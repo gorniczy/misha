cfb2a78c392901f507d029638f4f888719c073e6
gorniczy <denysgornichar@gmail.com>
2019-02-16 15:46:33 +0100

    add animation function to navList

--- script/script.js
+++ script/script.js
@@ -11,6 +11,77 @@ const navBubbles = document.getElementsByClassName('nav-bubbles')[0];
 const navContact = document.getElementsByClassName('nav-contact')[0];
 const navArrow = document.getElementsByClassName('arrow')[0];
 
+// navigation list animation for portrait orientation
+
+function moveList(position, distance, duration) {
+  let start = null;
+  function animation(currentTime) {
+    if (start === null) start = currentTime;
+    const timeElapsed = currentTime - start;
+    const negativePosition = position < 0;
+    const currentPosition = negativePosition ? position + distance * timeElapsed / duration : position - distance * timeElapsed / duration;
+    const wantedPosition = negativePosition ? Math.min(currentPosition, 0) : Math.max(currentPosition, -70);
+    navList.style.marginRight = "" + wantedPosition + "%";
+    console.log(wantedPosition);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  };
+  requestAnimationFrame(animation);
+};
+
+// toggle mobile nav button and nav list
+
+const navVisibility = {
+  showButton: true,
+  showNavlist: false
+};
+
+function toggleButton() {
+  const portrait = window.matchMedia("(orientation: portrait)").matches;
+  if (portrait) {
+    navButton.style.display = navVisibility.showButton ? "none" : "inherit";
+    navVisibility.showButton = !navVisibility.showButton;
+  };
+  return null;
+};
+
+function toggleNavList() {
+  const portrait = window.matchMedia("(orientation: portrait)").matches;
+  if (portrait) {
+    navVisibility.showNavlist ? moveList(0, 70, 500) : moveList(-70, 70, 500);
+    navVisibility.showNavlist = !navVisibility.showNavlist;
+  };
+  return null;
+};
+
+// return to defaults on window resize
+
+const changeOrientation = {
+  toPortrait: !portrait,
+  toLandscape: portrait
+};
+
+function defaultPortrait() {
+  if (changeOrientation.toPortrait) {
+    navButton.style.display = "inherit";
+    navList.style.marginRight = "-70%";
+    changeOrientation.toPortrait = false;
+    changeOrientation.toLandscape = true;
+    navVisibility.showButton = true;
+    navVisibility.showNavlist = false;
+  };
+  return null;
+};
+
+function defaultLandscape() {
+  if (changeOrientation.toLandscape) {
+    navButton.style.display = "none";
+    navList.style.marginRight = "0";
+    changeOrientation.toLandscape = false;
+    changeOrientation.toPortrait = true;
+  };
+  return null;
+};
+
 // smooth scroll animation
 
 function smoothScroll(target, duration) {
@@ -27,80 +98,26 @@ function smoothScroll(target, duration) {
     const wantedPosition = startPosition < scrollToY ? Math.min(currentPosition, scrollToY) : Math.max(currentPosition, scrollToY);
     window.scrollTo(0, wantedPosition);
     if (timeElapsed < duration) requestAnimationFrame(animation);
-  }
+  };
   requestAnimationFrame(animation);
-}
-
-// toggle mobile nav button and nav list
-
-const navVisibility = {
-  showButton: true,
-  showNavlist: false
-}
-
-function toggleButton() {
-  const portrait = window.matchMedia("(orientation: portrait)").matches;
-  if (portrait) {
-    navButton.style.display = navVisibility.showButton ? "none" : "inherit";
-    navVisibility.showButton = !navVisibility.showButton;
-  }
-  return null;
-}
-
-function toggleNavList() {
-  const portrait = window.matchMedia("(orientation: portrait)").matches;
-  if (portrait) {
-    navList.style.marginRight = navVisibility.showNavlist ? "-70%" : "0";
-    navVisibility.showNavlist = !navVisibility.showNavlist;
-  }
-  return null;
-}
-
-// return to defaults on window resize
-
-const changeOrientation = {
-  toPortrait: !portrait,
-  toLandscape: portrait
-}
-
-function defaultPortrait() {
-  if (changeOrientation.toPortrait) {
-    navButton.style.display = "inherit";
-    navList.style.marginRight = "-70%";
-    changeOrientation.toPortrait = false;
-    changeOrientation.toLandscape = true;
-    navVisibility.showButton = true;
-    navVisibility.showNavlist = false;
-  }
-  return null;
-}
-
-function defaultLandscape() {
-  if (changeOrientation.toLandscape) {
-    navButton.style.display = "none";
-    navList.style.marginRight = "0";
-    changeOrientation.toLandscape = false;
-    changeOrientation.toPortrait = true;
-  }
-  return null;
-}
+};
 
 // assign event listeners to functions
 
 navMisha.addEventListener('click', function() {
   smoothScroll('#nav-1', 500);
-  toggleButton();
   toggleNavList();
+  setTimeout(toggleButton, 500);
 });
 navBubbles.addEventListener('click', function() {
   smoothScroll('#nav-2', 500);
-  toggleButton();
   toggleNavList();
+  setTimeout(toggleButton, 500);
 });
 navContact.addEventListener('click', function() {
   smoothScroll('#nav-3', 500);
-  toggleButton();
   toggleNavList();
+  setTimeout(toggleButton, 500);
 });
 
 navButton.addEventListener('click', function() {
